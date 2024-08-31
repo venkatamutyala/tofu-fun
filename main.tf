18074d7dc961fc9b60ff862ac5a37b22109bf5bd
Venkat <venkata@venkatamutyala.com>
2024-08-31 19:25:10 +0000

    yolo

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.2"
+    }
+  }
+}
+
+
+resource "random_bytes" "jwt_secret" {
+  length = 64
+}
