1f6b3df3ed1631cf7fba63163c1bec9e5d5aa9fb
Venkat <venkata@venkatamutyala.com>
2024-08-31 21:29:35 +0000

    test

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
-provider "random" {}
+terraform {
+  required_providers {
+    hashicorp = {
+      source = "hashicorp/random"
+      version = "v3.6.2"
+    }
+  }
+}
+
+
+
+provider "hashicorp" {
+  # Configuration options
+}
+
+
 
 resource "random_bytes" "jwt_secret" {
   length = 64
